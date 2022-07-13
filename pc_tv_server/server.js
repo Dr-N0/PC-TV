0058b7f9cbaaabce06f5fb4d0e33d492601187d6
Dr-N0 <beckettjenen@gmail.com>
2022-07-13 19:57:16 -0400

    Server time

---
+++ pc_tv_server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const connectDB = require("./db");
+const app = express();
+
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./Auth/route"));
+
+const server = app.listen(PORT, () =>
+  console.log(`Server Connected to port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err) => {
+  console.log(`An error occurred: ${err.message}`);
+  server.close(() => process.exit(1));
+});
