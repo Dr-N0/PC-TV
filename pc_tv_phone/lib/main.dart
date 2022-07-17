fef31ea64f0e18a34787e858fa0539be39fa46ff
Dr-N0 <beckettjenen@gmail.com>
2022-07-17 17:33:06 -0400

    App basic format + desktop robotjs package lock

--- pc_tv_phone/lib/main.dart
+++ pc_tv_phone/lib/main.dart
@@ -1,24 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:pc_tv_phone/LoginPage.dart';
+import 'package:pc_tv_phone/SettingsPage.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+      debugShowCheckedModeBanner: false,
+      title: 'PC TV',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var appBarHeight = AppBar().preferredSize.height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PC TV'),
+        backgroundColor: Colors.purple,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.volume_up),
+            tooltip: 'Computer Sound Info',
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.battery_full),
+            tooltip: 'Battery Levels',
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.analytics),
+            tooltip: 'Computer Performance',
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.change_circle),
+            tooltip: 'Change Computer',
+            onPressed: () {},
+          ),
+          Theme(
+            data: Theme.of(context).copyWith(
+                dividerColor: Colors.black,
+                iconTheme: const IconThemeData(color: Colors.white)),
+            child: PopupMenuButton<int>(
+              color: Colors.white,
+              offset: Offset(0.0, appBarHeight + 10),
+              itemBuilder: (context) => [
+                const PopupMenuItem<int>(value: 0, child: Text("Settings")),
+                const PopupMenuItem<int>(
+                    value: 1, child: Text("Privacy Policy page")),
+                const PopupMenuDivider(),
+                PopupMenuItem<int>(
+                    value: 2,
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.logout,
+                          color: Colors.red,
+                        ),
+                        SizedBox(
+                          width: 7,
+                        ),
+                        Text("Logout")
+                      ],
+                    )),
+              ],
+              onSelected: (item) => selectedItem(context, item),
+            ),
+          ),
+        ],
+        elevation: 0.0,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: <Color>[
+              Color.fromARGB(255, 255, 255, 255),
+              Color(0xff5b0060),
+              Color(0xff870160),
+              Color(0xffac255e),
+              Color(0xffca485c),
+              Color(0xffe16b5c),
+              Color(0xfff39060),
+              Color(0xffffb56b),
+            ],
+            tileMode: TileMode.mirror,
+          ),
         ),
       ),
     );
+  }
+
+  void selectedItem(BuildContext context, item) {
+    switch (item) {
+      case 0:
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const SettingsPage()));
+        break;
+      case 1:
+        print("Privacy Clicked");
+        break;
+      case 2:
+        print("User Logged out");
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => const LoginPage()),
+            (route) => false);
+        break;
+    }
   }
 }
